7800ae2b93fcbfcd9954376a21549ef8e4130acc
Sh1r0w <k4leth@gmail.com>
2021-06-12 21:50:03 +0200

    first commit

---
+++ backend/models/Thing.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+    userId: { type: String, required: true},
+    name: { type: String, required: true},
+    manufacturer: { type: String, required: true},
+    description: { type: String, required: true},
+    mainPepper: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+    heat: { type: String, required: true},
+ //   userLiked: { type: String },
+  //  usersDisliked: { type: String }
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
