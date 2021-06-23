80964997f75de4027f5d10c73c16a951ee4a88e4
Sh1r0w <k4leth@gmail.com>
2021-06-23 21:43:50 +0200

    update 5 2306 like on

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -2,27 +2,42 @@ const Thing = require('../models/Thing');
 
 exports.newLike = (req, res, next) => {
     let likes = req.body.like;
-    const user = req.body.userId;
-    const userLiked = req.params.usersLiked
+    let user = req.body.userId;
 
-    if(likes === 1 && !userLiked.find(usersLiked => usersLiked = user)){
-  Thing.updateOne({ _id: req.params.id }, { $push:{usersLiked: user}, $inc: {likes: 1}})
-    .then(() => res.status(201).json({ message: 'Like updated successfully!' })
-    )
-    .catch(error => { res.status(400).json({ error: error })
-    });
-    console.log(user)
-}else if (likes === -1){
-    Thing.updateOne({ _id: req.params.id }, { $push:{usersDisliked: user}, $inc: {dislikes: 1}})
-    .then(() => res.status(201).json({ message: 'Dislike updated successfully!' })
-    )
-    .catch(error => { res.status(400).json({ error: error })
-    });
-} else {
-    Thing.updateOne({ _id: req.params.id }, { $pull:{usersDisliked: user}, $pull: {usersLiked: user}, $inc: {dislikes: 1}})
-    .then(() => res.status(201).json({ message: 'Reset Like successfully!' })
-    )
-    .catch(error => { res.status(400).json({ error: error })
-    });
-}
-};
+
+    if (likes === 1) {
+        Thing.updateOne({ _id: req.params.id }, { $push: { usersLiked: user }, $inc: { likes: 1 }, _id: req.params.id })
+            .then(() => res.status(201).json({ message: 'Like updated successfully!' })
+            )
+            .catch(error => {
+                res.status(400).json({ error: error })
+            });
+    } else if (likes === -1) {
+        Thing.updateOne({ _id: req.params.id }, { $push: { usersDisliked: user }, $inc: { dislikes: 1 } })
+            .then(() => res.status(201).json({ message: 'Dislike updated successfully!' })
+            )
+            .catch(error => {
+                res.status(400).json({ error: error })
+            });
+    } else if (likes === 0) {
+        Thing.findOne({ _id: req.params.id })
+            .then((thing) => {
+                if (thing.usersLiked.find( user => user === user)) {
+                    Thing.updateOne({ _id: req.params.id }, { $pull: { usersLiked: user }, $inc: { likes: -1 } })
+                        .then(() => res.status(201).json({ message: 'Reset Like successfully!' })
+                        )
+                        .catch(error => {
+                            res.status(400).json({ error: error })
+                        });
+                } else if (thing.usersDisliked.find( user => user === user)) {
+                    Thing.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: user }, $inc: { dislikes: -1 } })
+                        .then(() => res.status(201).json({ message: 'Reset Like successfully!' })
+                        )
+                        .catch(error => {
+                            res.status(400).json({ error: error })
+                        });
+                }
+            })
+            .catch(error => res.status(400).json({ error: error })
+            )};
+    };
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff');
+const likeCtrl = require('../controllers/like');
 
 
 router.post('/', auth, multer, stuffCtrl.createThing);
@@ -12,7 +13,7 @@ router.get('/', auth, stuffCtrl.getAllStuff);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
-  
+router.post('/:id/like', auth, likeCtrl.newLike);  
 
 
 module.exports = router;
